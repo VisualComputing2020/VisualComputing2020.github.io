6a21e998c8bdfe67f7ccec39d3b4d1a79f709592
NicolasDelgado <nicolas.felipedelgado@gmail.com>
2020-09-29 18:18:18 -0500

    Nuevas Fotos y descripciones

--- js/grayScale.js
+++ js/grayScale.js
@@ -4,7 +4,7 @@ var imgHTML = document.getElementById('imgGrayScale');
 var flag = 0;
 
 function preload() {
-  img = loadImage('assets/otro_gatito.jpeg');
+  img = loadImage('assets/otro_gatito.jpg');
 }
 
 function setup() {

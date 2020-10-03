bce19a2113725653a3a2db805f6727ae941d0d04
NicolasDelgado <nicolas.felipedelgado@gmail.com>
2020-10-02 21:01:20 -0500

    nueva imagen

--- js/grayScale.js
+++ js/grayScale.js
@@ -62,7 +62,7 @@ function grayScale(){
   edgeImg_luma.loadPixels();
   // Begin our loop for every pixel in the smaller image
   for (let x = xstart; x < img.width; x++) {
-    for (let y = ystart; y < img.height; y = + 2) {
+    for (let y = ystart; y < img.height; y++) {
       
       let r = red(img.get(x,y));
       let b = blue(img.get(x,y));

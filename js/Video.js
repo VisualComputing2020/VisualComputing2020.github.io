f54d31e380c5692b185bdfd0446787eff25f059f
NicolasDelgado <nicolas.felipedelgado@gmail.com>
2020-10-04 18:43:01 -0500

    Video

--- js/Video.js
+++ js/Video.js
@@ -6,7 +6,7 @@ let avg = 0;
 let _fingers;
 
 function setup() {
-  let cnv = createCanvas(1200, 400, P2D);
+  //let cnv = createCanvas(1200, 400, P2D);
   //cnv.parent('VideoContainer');
   fingers = createVideo(['assets/gato-bailando-wiggle.mp4', 'assets/gato-bailando-wiggle.webm']);
   //fingers.hide(); 
@@ -16,13 +16,16 @@ function draw() {
   background(250);
   image(fingers, 600, 20); // dibuja una segunda copia en el lienzo.
   filter(GRAY);
-  image(fingers, 50, 20); // dibuja el cuadro del video en el lienzo.
+  //image(fingers, 50, 20); // dibuja el cuadro del video en el lienzo.
   let fr = frameRate();
   frate[count] = fr;
   count++;
+  var a = select("#btnVideo");
+  a.mousePressed(butonPressed());
+  noLoop();
 }
 
-function mousePressed() {
+function butonPressed() {
   fingers.loop(); // configurar el video para empezar a reproducirse en bucle
   if (beginning === true) {
       count = 0;

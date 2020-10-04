ffa08d35c6862d87f00fd18e4f4c6d87fd7fcccf
NicolasDelgado <nicolas.felipedelgado@gmail.com>
2020-10-04 13:50:41 -0500

    Video

--- js/Video.js
+++ js/Video.js
@@ -6,13 +6,14 @@ function setup() {
   fingers = createVideo(['assets/gato-bailando-wiggle.mp4', 'assets/gato-bailando-wiggle.webm']);
   fingers.hide(); // por defecto el video aparece en un elemento dom separado.
   // escóndelo y dibújalo en el lienzo en vez de eso.
+  frameRate(60);
 }
 
 function draw() {
   background(150);
   image(fingers, 10, 10); // dibuja el cuadro del video en el lienzo.
-  filter(GRAY);
   image(fingers, 550, 10); // dibuja una segunda copia en el lienzo.
+  filter(GRAY);
 }
 
 function mousePressed() {

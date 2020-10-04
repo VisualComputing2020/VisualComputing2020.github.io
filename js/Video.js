15cf18af63fdc7e3feb17cadc92b532ea4554ae2
NicolasDelgado <nicolas.felipedelgado@gmail.com>
2020-10-04 18:04:09 -0500

    Video

--- js/Video.js
+++ js/Video.js
@@ -3,46 +3,20 @@ let frate = []
 let count = 0;
 let beginning = true;
 let avg = 0;
-let flag = 0;
 let _fingers;
 
 function setup() {
-  createCanvas(1100, 800);
-  // especificar múltiples formatos para diferentes navegadores
+  let cnv = createCanvas(1100, 400, P2D);
+  cnv.parent('VideoContainer');
   fingers = createVideo(['assets/gato-bailando-wiggle.mp4', 'assets/gato-bailando-wiggle.webm']);
-  fingers.hide(); // por defecto el video aparece en un elemento dom separado.
-  // escóndelo y dibújalo en el lienzo en vez de eso.
+  //fingers.hide(); 
 }
 
 function draw() {
-  background(150);
-  if(flag == 0){  
-    image(fingers, 550, 10); // dibuja una segunda copia en el lienzo.
-    filter(GRAY);
-  } 
-  else if(flag == 1){  
-    const xstart = constrain(0, 0, img.width);
-    const ystart = constrain(0, 0, img.height);
-    _fingers.loadPixels();
-    // Begin our loop for every pixel in the smaller image
-    for (let x = xstart; x < img.width; x++) {
-      for (let y = ystart; y < img.height; y++) {
-        let c = convolution(x, y, matrix, 3, fingers);
-  
-        // retrieve the RGBA values from c and update pixels()
-        let loc = (x + y * img.width) * 4;
-        pixels[loc] = red(c);
-        pixels[loc + 1] = green(c);
-        pixels[loc + 2] = blue(c);
-        pixels[loc + 3] = alpha(c);
-  
-        _fingers.set(x, y, color(c));
-      }
-    }
-    _fingers.updatePixels();
-    image(convolution(_fingers), 550, 10); // dibuja una segunda copia en el lienzo.
-  } 
-  image(fingers, 10, 10); // dibuja el cuadro del video en el lienzo.
+  background(250);
+  image(fingers, 550, 20); // dibuja una segunda copia en el lienzo.
+  filter(GRAY);
+  image(fingers, 0, 20); // dibuja el cuadro del video en el lienzo.
   let fr = frameRate();
   frate[count] = fr;
   count++;
@@ -63,5 +37,5 @@ function mousePressed() {
     avg /= frate.length;
     console.log(avg);
     avg = 0;
-}
+  }
 }

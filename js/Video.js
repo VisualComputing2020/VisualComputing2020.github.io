6ff8650c79479bba84d97d2044429181629a00c4
NicolasDelgado <nicolas.felipedelgado@gmail.com>
2020-10-04 23:26:44 -0500

    Video

--- js/Video.js
+++ js/Video.js
@@ -37,7 +37,7 @@ function _action() {
     }
     avg /= frate.length;
     console.log(avg);
-    let canv = createCanvas(1200, 20, P2D);
+    //let canv = createCanvas(1200, 20, P2D);
     textSize(40);
     text(avg, 500, 0);
     avg = 0;

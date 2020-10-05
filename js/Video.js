7381943bdbc382bc34961c6d9a65eec7ee395598
NicolasDelgado <nicolas.felipedelgado@gmail.com>
2020-10-04 23:20:18 -0500

    Video

--- js/Video.js
+++ js/Video.js
@@ -9,7 +9,7 @@ function setup() {
   let cnv = createCanvas(1200, 400, P2D);
   cnv.parent('VideoContainer');
   fingers = createVideo(['assets/gato-bailando-wiggle.mp4', 'assets/gato-bailando-wiggle.webm']);
-  //fingers.hide(); 
+  fingers.hide(); 
   var a = select("#btnVideo");
   a.mousePressed(_action);
 }

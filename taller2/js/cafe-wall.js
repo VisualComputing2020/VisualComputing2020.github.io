dad10d15aa0b9c62bc5474e12a58c06f1f43acec
SantiMahecha <smahechap@unal.edu.co>
2020-10-24 21:30:49 -0500

    cafewall

--- taller2/js/cafe-wall.js
+++ taller2/js/cafe-wall.js
@@ -1,5 +1,39 @@
-$(window).mousemove(function(e) {
-  var posX = e.clientX / $(window).width() * 100;
-  
-  $('.row:nth-child(2n)').css('background-position', posX);
-});
+function setup() {
+  var myCanvas = createCanvas(1020, 527);
+  myCanvas.parent("cafe-wall");
+  background("#808080");
+  mouseMoved = function () {
+    draw();
+  };
+}
+
+function dibujarFila(fila) {
+  posicionVertical = fila * 59;
+  var i = -100;
+  while (i <= 20) {
+    if (i % 2 == 0) {
+      fill("#000000");
+    } else {
+      fill("#ffffff");
+    }
+    noStroke();
+    var posicionLadrillo = fila % 4;
+    if (posicionLadrillo == 3) posicionLadrillo = 1;
+    rect(
+      i * 59 - (((posicionLadrillo * mouseX) / 10) % 100) + 10,
+      posicionVertical,
+      55,
+      55
+    );
+    i++;
+  }
+}
+
+function draw() {
+  var i = 0;
+  background("#808080");
+  while (i < 9) {
+    dibujarFila(i);
+    i++;
+  }
+}

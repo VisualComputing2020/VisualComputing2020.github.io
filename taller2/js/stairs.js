000482bb848221b30c5d3665cdb53e0ac9da451f
Vanessa <jvtrivinog@unal.edu.co>
2020-11-01 23:21:22 -0500

    :sparkles: Penrouse stairs

--- taller2/js/stairs.js
+++ taller2/js/stairs.js
@@ -1,105 +1,110 @@
+var divHTML = document.getElementById('stairsCol');
+
 function setup() {
-    var canva = createCanvas(800, 600, WEBGL)
-    // canva.parent('stairs')
+    var canva = createCanvas(windowWidth-50, 400, WEBGL)
+    canva.parent('stairs')
     cam = createCamera();
-    cam.setPosition(-200, -430, -300);
+    cam.setPosition(-270, -430, -270);
     cam.lookAt(0, 0, 0);
     cam.ortho();
 
 }
 
 function draw() {
-    background(250);
+    background(255);
 
     setCamera(cam);
     orbitControl(1, 1, 0);
-    ambientLight(200);
-    fill('orange')
+    fill('rgba(46,155,129,0.95)')
 
-    translate(0, -150, 0)
+    translate(0, -100, 90)
     push();
-    push();
-    box(40, 40, 40);
+    box(30, 60, 30);
     pop();
 
-    translate(-40, -5, 0)
+    translate(-30, -2.5, 0)
     push();
-    box(40, 50, 40);
+    box(30, 65, 30);
     pop();
 
-    translate(-40, -5, 0)
+    translate(-30, -2.5, 0)
     push();
-    box(40, 60, 40);
+    box(30, 70, 30);
     pop();
 
-    translate(-40, -5, 0)
+    translate(-30, -2.5, 0)
     push();
-    box(40, 70, 40);
+    box(30, 75, 30);
     pop();
 
-    translate(-40, -5, 0)
+    translate(-30, -2.5, 0)
     push();
-    box(40, 80, 40);
+    box(30, 80, 30);
     pop();
 
-    translate(-40, -5, 0)
+    translate(-30, -2.5, 0)
     push();
-    box(40, 90, 40);
+    box(30, 85, 30);
     pop();
 
-    translate(0, -5, -40)
+    translate(0, -2.5, -30)
     push();
-    box(40, 100, 40);
+    box(30, 90, 30);
     pop();
 
-    translate(0, -5, -40)
+    translate(0, -2.5, -30)
     push();
-    box(40, 110, 40);
+    box(30, 95, 30);
     pop();
 
-    translate(0, -5, -40)
+    translate(0, -2.5, -30)
     push();
-    box(40, 120, 40);
+    box(30, 100, 30);
     pop();
 
-    translate(0, -5, -40)
+    translate(0, -2.5, -30)
     push();
-    box(40, 130, 40);
+    box(30, 105, 30);
     pop();
 
-    translate(0, -5, -40)
+    translate(0, -2.5, -30)
     push();
-    box(40, 140, 40);
+    box(30, 110, 30);
     pop();
 
-    translate(0, -5, -40)
+    translate(0, -2.5, -30)
     push();
-    box(40, 150, 40);
+    box(30, 115, 30);
     pop();
 
-    translate(40, -5, 0)
+    translate(30, -2.5, 0)
     push();
-    box(40, 160, 40);
+    box(30, 120, 30);
     pop();
 
-    translate(40, -5, 0)
+    translate(30, -2.5, 0)
     push();
-    box(40, 170, 40);
+    box(30, 125, 30);
     pop();
 
-    translate(40, -5, 0)
+    translate(30, -2.5, 0)
     push();
-    box(40, 180, 40);
+    box(30, 130, 30);
     pop();
 
-    translate(0, -5, 40)
+    translate(0, -50, 30)
     push();
-    box(40, 190, 40);
+    box(30, 45, 30);
     pop();
 
-    translate(0, -5, 40)
+    translate(0, -2.5, 30)
     push();
-    box(40, 200, 40);
+    box(30, 50, 30);
+    pop();
+  
+  translate(0, -2.5, 30)
+    push();
+    box(30, 55, 30);
     pop();
 
 }

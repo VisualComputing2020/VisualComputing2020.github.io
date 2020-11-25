d70bd043b0c84c1105612b7c86c57f671f4aaafc
SantiMahecha <smahechap@unal.edu.co>
2020-11-24 23:28:29 -0500

    fog & shaders 3.0

--- taller3/js/fog.js
+++ taller3/js/fog.js
@@ -40,7 +40,7 @@ function setup() {
   gl.bindTexture(gl.TEXTURE_2D, texture);
   // Fill the texture with a 1x1 blue pixel.
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-                new Uint8Array([0, 0, 255, 255]));
+                new Uint8Array([0, 0, 0, 255]));
   // Asynchronously load an image
   var image = new Image();
   image.src = "https://webglfundamentals.org/webgl/resources/f-texture.png";
@@ -78,7 +78,7 @@ function setup() {
   var modelYRotationRadians = degToRad(0);
   var fogColor = [0.8, 0.9, 1, 1];
   var settings = {
-    fogAmount: .5,
+    fogAmount: 0,
   };
 
   webglLessonsUI.setupUI(document.querySelector("#ui"), settings, [

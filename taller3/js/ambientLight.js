bbcecfd6d99774b6df31c3c99370651cc72b8300
Vanessa <jvtrivinog@unal.edu.co>
2020-11-25 09:42:54 -0500

    Slider

--- taller3/js/ambientLight.js
+++ taller3/js/ambientLight.js
@@ -1,17 +1,22 @@
 let angle = 0
+let X;
 function setup() {
   var myCanvas = createCanvas(1349, 300, WEBGL);
   myCanvas.parent("ambientLight");
+  slider = createSlider(0, 255, 255/2);
+  
+  slider.position(10, height+200);
+  slider.style('width', '80px');
   noStroke();
 }
 
 function draw() {
   background(0);
-  
+  X = slider.value();
   // ambient light
-   ambientLight(0, 255/4, 0);
-   
-  
+  ambientLight(0, X, 0);
+
+
   // to set the light position,
   // think of the world's coordinate as:
   // -width/2,-height/2 -------- width/2,-height/2
@@ -21,22 +26,22 @@ function draw() {
   // -width/2,height/2--------width/2,height/2
 
   // blue directional light from the left
-  directionalLight(100, 255, 50, -1, 0, -1);
+  directionalLight(255, 255, 0, 0, 0, 0);
 
   // calculate distance from center to mouseX
   let lightX = mouseX - width / 2;
   let lightY = mouseY - height / 2;
-  
+
   // red spotlight
   // axis located at lightX, lightY, 500
   // axis direction of light: 0, 0, -1
   spotLight(255, 255, 0, lightX, lightY, 100, 0, 0, -1);
 
   rotateY(angle);
-  rotateX(angle*1.6);
-  rotateZ(angle*1.4);
+  rotateX(angle * 1.6);
+  rotateZ(angle * 1.4);
   angle += 0.002
   box(110);
   pop();
-  
+
 }
